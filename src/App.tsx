13929e51470d3ce7cf6b96dfd37b9e56cc80c27c
Gustavo Galote <gusgalot3g@gmail.com>
2022-08-04 02:15:21 -0300

    feat: Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import { MantineProvider } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
 import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
 import { Routes } from './routes';
+
+const tabs = [
+  'Home',
+  'Orders',
+  'Education',
+  'Community',
+  'Forums',
+  'Support',
+  'Account',
+  'Helpdesk',
+];
+
+const user = {
+  name: 'John Doe',
+  image: 'https://github.com/gusgalote.png',
+};
 
 export function App() {
   return (
     <BrowserRouter>
       <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Navbar user={user} tabs={tabs} />
         <Routes />
         <Footer />
       </MantineProvider>
--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   SimpleGrid,
 } from '@mantine/core';
+import { Link } from 'react-router-dom';
 import image from './image.svg';
 
 const useStyles = createStyles((theme) => ({
@@ -63,14 +64,16 @@ export function NotFound() {
             the address, or the page has been moved to another URL. If you think
             this is an error contact support.
           </Text>
-          <Button
-            variant="outline"
-            size="md"
-            mt="xl"
-            className={classes.control}
-          >
-            Get back to home page
-          </Button>
+          <Link to={'/'}>
+            <Button
+              variant="outline"
+              size="md"
+              mt="xl"
+              className={classes.control}
+            >
+              Get back to home page
+            </Button>
+          </Link>
         </div>
         <Image src={image} className={classes.desktopImage} />
       </SimpleGrid>
