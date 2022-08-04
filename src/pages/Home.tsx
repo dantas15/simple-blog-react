294a40908230e57c2531f9889b9b2c4545b6b502
Gustavo Galote <gusgalot3g@gmail.com>
2022-08-04 02:34:47 -0300

    feat: Blog Cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
+import { Container, SimpleGrid } from '@mantine/core';
+import { ImageCard } from '../components/ImageCard';
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 export function Home() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Container>
+      <SimpleGrid cols={3}>
+        {arr.map((item, index) => (
+          <ImageCard
+            author="gus"
+            comments={2}
+            image={`https://source.unsplash.com/random/?Programming&${index}`}
+            link={'/'}
+            title="Programming"
+            key={item}
+          />
+        ))}
+      </SimpleGrid>
+    </Container>
   );
 }
